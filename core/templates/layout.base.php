625982fd5cc65d27996ef8f0708812cced59403a
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-10-20 01:54:38 +0330

    feat: apply direction to body of html layouts

--- core/templates/layout.base.php
+++ core/templates/layout.base.php
@@ -14,7 +14,7 @@
 		<?php emit_script_loading_tags($_); ?>
 		<?php print_unescaped($_['headers']); ?>
 	</head>
-	<body id="body-public" class="layout-base">
+	<body id="body-public" class="layout-base" dir="<?php p($_['direction']) ?>">
 		<?php include 'layout.noscript.warning.php'; ?>
 		<?php foreach ($_['initialStates'] as $app => $initialState) { ?>
 			<input type="hidden" id="initial-state-<?php p($app); ?>" value="<?php p(base64_encode($initialState)); ?>">
